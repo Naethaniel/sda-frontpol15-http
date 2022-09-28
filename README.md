435665286fb9eea60d783ba3c7fd7fa95d84482f
naethaniel <bljuuzeff@gmail.com>
2022-09-28 22:19:40 +0200

    Przygotowanie prostrzej wersji zadania

--- README.md
+++ README.md
@@ -2,24 +2,35 @@
 
 Przy pomocy API dostepnego pod adresem
 ```https://jsonplaceholder.typicode.com```
-Przygotowanie CRUD'a odnośnie postów i komentarzy do tych postów.
-Aplikacja powinna być napisana w styl aplikacji SPA. Oznacza to, że strona powinna działać jako jedna strona bez potrzeby pobierania kolejnych plików HTML.
 Proszę o przygotowanie następujących funkcjonalności:
 
-1. Lista postów:
-    * Pobieranie listy postów za pomocą metody GET dostepnych pod adresem: ```https://jsonplaceholder.typicode.com/posts```
-    * Po wejściu na stronę główną posty powinny się załadować i wyświetlić jako lista
-    * Możliwość usuwania postów (przy pomocy guzika na liście i odpowiedniego zapytania)
-    * Możliwość edycji postów (przy pomocy guzika i modala i odpowiedniego zapytania)
-    * Możliwość dodawania postów (przy pomocy guzika i modala i odpowiedniego zapytania)
-2. Lista komentarzy:
-    * Pobieranie listy komentarzy do danego postu za pomoca metody GET, które dostępne są pod adresem: ```https://jsonplaceholder.typicode.com/comments?postId=XXX``` gdzie XXX oznacza ID posta
-    * Komentarze pojawiają się na kliknieciu w danego posta (jako lista poniżej)
-    * Możliwość dodania komentarza (przy pomocy guzika na liście i odpowiedniego zapytania)
-    * Możliwość usunięcia komentarza (przy pomocy guzika na liście i odpowiedniego zapytania)
-3. (Bonusowo) Zaimplementowanie obsługi zalogowanego użytkownika przy pomocy ciasteczka
-    * Strona główna dopiero dostepna po zalogowaniu
-    * Jeżeli użytkownik nie jest zalogowany to wyświetlamy formularz do nadania nazwy użytkownika
-    * Po wpisaniu nazwy użytkownik uzyskuje dostęp do aplikacji.
-    * Jeżeli użytkownik ponownie wejdzie na strone nie ma obowiązku podawania znowu nazwy użytkownika (tutaj mechanizm ciasteczek musi zostać obslużony)
-    * Dodanie guzika który usunie ciasteczko
+1. Lista postów (/posts):
+    * Implementacja obsługi zapytań:
+      * Pobranie listy postów
+      * Edycja posta
+      * Dodanie posta
+      * Usunięcie posta
+2. Lista komentarzy (/comments):
+    * Implementacja obsługi zapytań:
+      * Pobranie listy komentarzy dla konkretnego postu
+      * Moliwosc dodania komentarza do konkretnego postu
+      * Mozliwosc usuniecia konkretnego komentarza dla konkretnego postu
+
+
+Wszystko powinno byc zaimplementowane w odpowiedniej klasie i przy uzyciu funkcji `fetch`.
+
+Zadanie dodatkowe:
+1. Lista uzytkownikow (/users):
+    * Pobieranie listy uzytkownikow
+    * Dodanie uzytkownika
+    * Usuniecie uzytkownika
+    * Edycja uzytkownika
+2. Przygotowac obsługę błędów:
+   1. Lista postow:
+      * Podczas dodawania postu przygotowac kod aby obsluzyl status http 400 (Bad request). Niestety API nie pozwala zwrocic dowolnych statusow takze to zadanie jest "na sucho"
+   2. Lista uzytkownikow
+      * Podczas edycji uzytkownika przygotowac kod aby obsluzyl status 401 Unauthorized. Niestety API nie pozwala zwrocic dowolnych statusow takze to zadanie jest na "sucho"
+
+
+**UWAGA**
+"na sucho" oznacza tutaj brak mozliwosci uruchomienia kodu poniewaz on sie nigdy nie wykona (z powodu ze ten status nigdy nie zostanie zwrocony w przypadku API w tym zadaniu)
--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,169 @@
-import { PostsController } from './controllers/posts.js';
-import { CommentsController } from './controllers/comments.js';
-import { PostsModel } from './models/posts.js';
-import { CommentsModel } from './models/comments.js';
+class Comments {
+
+    // Krok(13)
+    constructor() {
+        console.log('Comments instance created');
+    }
+
+    // Pobranie wszystkich komentarzy dla konkretnego postu
+    getAllComments(postId) {
+        // Adres zasobu
+        const URL = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+        // const URL = `https://jsonplaceholder.typicode.com/posts/comments?postId=${postId}`;
+        // Wykonanie zapytania
+        return fetch(URL) 
+            // Zwrocenie odpowiedzi w formie JSON
+            .then((response) => response.json());
+    }
+
+    addComment(comment) {
+        // Adres zasobu
+        const URL = `https://jsonplaceholder.typicode.com/posts/${comment.postId}/comments`;
+        // Cialo dla zapytania typu POST
+        const body = {
+            postId: comment.postId,
+            email: comment.email,
+            name: comment.name,
+            body: comment.body
+        };
+        // Ustawienie typu metody oraz ciala zapytania
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(body)
+        }
+        // Wykonanie zapytania
+        return fetch(URL, options)
+            //zwrocenie odpowiedzi w formie JSON
+            .then((response) => response.json());
+    }
+
+    deleteComment(commentId) {
+        // Adres zasobu
+        const URL = `https://jsonplaceholder.typicode.com/comments/${commentId}`;
+        // Ustawienie typu zapytania
+        const options = {
+            method: 'DELETE'
+        };
+        // Wykonanie zapytania
+        return fetch(URL, options).then((response) => response.json());
+    }
+}
+
+export class Posts {
+
+    // Krok (7)
+    constructor() {
+        console.log('Posts instance created');
+    }
+
+    // Wykonanie zapytania typu GET do API aby pobrać liste postów
+    getAllPosts() {
+        const URL = 'https://jsonplaceholder.typicode.com/posts';
+        // Krok (18) wywolanie funkcji fetch ktora domyslnie wykonuje zapytanie typu GET 
+        return fetch(URL) // Krok (19) asynchronicznie leci zapytanie typu GET do API moze potrwać np. 2-3s
+        .then((response) => response.json());  // Krok (20) po wykonaniu zapytania zostanie wywolana ta funckja, ktora przygotuje nam JSONa z postami za pomoca funkcji .json()
+    }
+
+    addPost(post) {
+        // Adres zasobu -> z dokumentacji
+        const URL = `https://jsonplaceholder.typicode.com/posts`;
+        // Przygotowanie ciala -> w dokumentacji powinno byc opisane co jest wymagane/opcjonalne
+        const body = {
+            title: post.title,
+            userId: post.userId,
+            body: post.body
+        };
+        // Ustawienie metody typu POST oraz przygotowanie ciala zapytania
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(body)
+        }
+        // Wykonanie zapytania pod dany adres z opcjami i zwrocenie wyniku jako JSON
+        return fetch(URL, options).then((response) => response.json());
+    }
+
+    deletePost(postId) {
+        const URL = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+        const options = {
+            method: 'DELETE'
+        };
+        return fetch(URL, options).then((response) => response.json());
+        // return fetch(URL, { method: 'DELETE' }).then((response) => response.json());
+    }
+
+    editPost(updatedPost) {
+        // Adres zasobu pod ktory nalezy wykonac zapytanie
+        const URL = `https://jsonplaceholder.typicode.com/posts/${updatedPost.id}`;
+        // Przygotowanie ciala zapytania - metoda PUT wymaga na nas zaktualizowania calego zasobu
+        const body = {
+            userId: updatedPost.userId,
+            id: updatedPost.id,
+            title: updatedPost.title,
+            body: updatedPost.body
+        };
+        // Opcje dla zapytania - ustawienie metody PUT oraz przygotowanie ciala zapytania
+        const options = {
+            method: 'PUT',
+            body: JSON.stringify(body)
+        };
+        // Wykonanie zapytania na podany adres z opcjami. Następnie zwrocenie odpowiedzi w formacie JSON
+        return fetch(URL, options).then((response) => response.json());
+    }
+
+    // Przyklad obslugi bledow z funkcja FETCH
+    errorHandlingExample() {
+        // Jakiś fukcyjny adres URL ktory powinien zwrocic nam jakis blad
+        const URL = 'https://www.iwillnotwork.com';
+        // Wywolanie fukcji fetch aby wykonac zapytanie typu GET
+        return fetch(URL)
+        // Jezeli zapytanie udalo sie wykonac przejdz do funkcji .then
+        .then((response) => {
+            // Jezeli status odpowiedzi jest w przedziale 200-299 wtedy response.ok bedzie true
+            if (response.ok) {
+                // Odpowiedz jest poprawna, zwroc ja w formacie JSON
+                return response.json();
+                // jezeli kod odpowiedzi wynosi 404 (Not Found) obsluz bład i wypisz do konsoli
+            } else if (response.status === 404) {
+                console.log('Nie znaleziono zasobu pod tym adresem');
+            }
+        })
+        // Jezeli nastapi problem z wykoneniem samego zapytania (nie mylic z bledem w sensie odpowiedzi z serwera)
+        // wykonaj funkcje .catch
+        // wywoluje sie np. przy problemie z CORS
+        .catch((error) => console.log('Złapany błąd', error));
+    }
+}
 
 
 const initApp = () => {
-    // Krok (5)
-    // Wyczyszczenie konsoli
     console.clear();
-    // Mamy tworzoną instancje klasy PostsController()
-    const postsController = new PostsController();
-    // Krok (10)
-    const commentsController = new CommentsController();
-    // Krok(15) załadowanie postów z API oraz stworzenie struktury w HTML
-    postsController.getAndViewAllPosts();
+    const posts = new Posts();
+    const comments = new Comments();
 
-    // Krok (22) UWAGA! Tak na prawde ten krok wykona sie po kroku (19)
-    // Dlaczego? Poniewa krok (19) zwraca Promise i dopiero jak zostanie wykonany wtedy wszsystkie funkcje .then(..) zostana wywolane
-    const commentsModel = new CommentsModel();
+    posts.getAllPosts();
+    posts.addPost({
+        userId: 5,
+        title: 'Post title',
+        body: 'Post body'
+    });
+    posts.deletePost(5);
+    posts.editPost({
+        postId: 2,
+        title: 'Tytul',
+        id: 5,
+        name: 'Nazwa',
+        email: 'test@test.com',
+        body: 'Body'
+    });
 
-    commentsModel.addComment({
+    comments.getAllComments();
+    comments.addComment({
         postId: 1,
         email: 'test@test.com',
         name: 'Nazwa',
         body: 'Jakies cialo'
     });
-
-    commentsModel.deleteComment(5);
+    comments.deleteComment(5);
 }
 
 document.addEventListener('readystatechange', () => { // Krok (4)
